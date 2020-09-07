dc744d32f0fb9ba66369c9bae6ea0a727f4b404d
NeoHuncho <william.guinaudie@gmail.com>
2020-09-07 21:49:31 +0200

    formated code+ sorted file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.scss';
 import marked from 'marked';
-const initialState=`this is a paragraph
+const initialState = `this is a paragraph
 
 **This is a bolded text**
 
@@ -28,36 +28,36 @@ let z=x+y;
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      text:initialState
-     }
-     this.handleChange= (event)=>{
+    this.state = {
+      text: initialState
+    }
+    this.handleChange = (event) => {
       this.setState({
-        text:event.target.value
+        text: event.target.value
       })
-     }
+    }
   }
-  render() { 
-    const {text}=this.state;
-    const markdown =marked(text, {breaks: true});
-    return ( 
-      
+  render() {
+    const { text } = this.state;
+    const markdown = marked(text, { breaks: true });
+    return (
+
       <div id="app">
-      <h1 className="text-center mt-4" id="title">Markdown Converter</h1><hr />
-      <section className="row">
-        <div className="col">
-          <h2 className="sub-title">Editor</h2><hr />
-           <textarea className="form-control" id="editor" value={text} onChange={this.handleChange}/></div>
+        <h1 className="text-center mt-4" id="title">Markdown Converter</h1><hr />
+        <section className="row">
           <div className="col">
-          <h2 id="titlePreview" className="sub-title">Preview</h2><hr />
-            <div  className="form-control" id="preview" dangerouslySetInnerHTML={{__html:markdown}}/>
-        </div>
-    </section>
-   </div>
-   );
+            <h2 className="sub-title">Editor</h2><hr />
+            <textarea className="form-control" id="editor" value={text} onChange={this.handleChange} /></div>
+          <div className="col">
+            <h2 id="titlePreview" className="sub-title">Preview</h2><hr />
+            <div className="form-control" id="preview" dangerouslySetInnerHTML={{ __html: markdown }} />
+          </div>
+        </section>
+      </div>
+    );
   }
 }
- 
+
 
 
 export default App;
